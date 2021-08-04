9b773afa9a4f6de23ee8e30f72abd4c4a9ee185a
Himanshu Pandey <himanshu21pandey@gmail.com>
2021-08-04 19:31:12 +0530

    Profile Section Completed

--- routes/account.js
+++ routes/account.js
@@ -47,4 +47,89 @@ router.get('/funds', (req,res) => {
         })
 })
 
+router.get('/holdings', (req,res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.fyers.in/api/v2/holdings',
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.holdings)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+router.get('/positions', (req,res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.fyers.in/api/v2/positions',
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.netPositions)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+router.get('/pnl', (req,res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.fyers.in/api/v2/positions',
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.overall)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+router.get('/orders', (req,res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.fyers.in/api/v2/orders',
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.orderBook)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+router.get('/tradebook', (req,res) => {
+    axios({
+        method: 'get',
+        url: 'https://api.fyers.in/api/v2/tradebook',
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.tradebook)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
 module.exports = router

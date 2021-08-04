d80279e6349d88d4e4bfdc14fc9f1c464d08b958
Himanshu Pandey <himanshu21pandey@gmail.com>
2021-08-05 01:35:23 +0530

    Data API

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ dotenv.config();
 
 const router_auth = require('./routes/auth.js')
 const router_account = require('./routes/account.js')
+const router_data = require('./routes/data.js')
 
 app.use(router_auth)
 app.use(router_account)
+app.use(router_data)
 
 //connection to db
 mongoose.set("useFindAndModify", false)
---
+++ routes/data.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const router = express.Router()
+bodyParser = require('body-parser').json()
+const app = require("../app")
+const axios = require('axios')
+const Token = require("../models/token")
+
+router.use(bodyParser)
+
+var token
+
+Token.findOne({}, {}, { sort: { 'created_at' : -1 } }, function(err, res) {
+    token = res.access_token
+})
+
+router.get('/historical', (req,res) => {
+    axios({
+        method: 'get',
+        url: `https://api.fyers.in/data-rest/v2/history/?symbol=${"NSE:SBIN-EQ"}&resolution=${"30"}&date_format=1&range_from=${"2021-08-01"}&range_to=${"2021-08-02"}&cont_flag=`,
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.candles)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+router.get('/quote', (req,res) => {
+    axios({
+        method: 'get',
+        url: `https://api.fyers.in/data-rest/v2/quotes/?symbols=${"NSE:SBIN-EQ"}`,
+        headers: {
+            "Authorization": `${process.env.appID}:${token}`
+        }   
+      })
+        .then((response) => {
+          res.status(200).send(response.data.candles)
+        })
+        .catch((e) => {
+            console.log(e)
+            res.status(500).send(e)
+        })
+})
+
+module.exports = router
